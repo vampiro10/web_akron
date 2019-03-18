f477ac78df8fa0b3c74503d09325020adbae96fe
Adrian Felix <felix.mar1995@hotmail.com>
2019-03-18 01:12:29 -0600

    Delete home and layout css js vendor

--- resources/views/layout2.blade.php
+++ resources/views/layout2.blade.php
@@ -5,7 +5,7 @@
         <head>
             <meta charset="utf-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <title>Akron e-commerce</title>
+            <title>Akron </title>
             <meta name="description" content="">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <meta name="robots" content="all,follow">
